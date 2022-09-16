29f89e0c6fde03a809c91ee566c7f676f515ae09
unknown <zhou.he1@northeastern.edu>
2022-09-16 13:55:43 -0700

    Add 141 solution

--- pythonLecture1_LinkedList/leetcode/Cycle_141.py
+++ pythonLecture1_LinkedList/leetcode/Cycle_141.py
@@ -23,7 +23,7 @@ class Solution141:
 
 if __name__ == "__main__":
 
-    solution141 = Solution141()
+    solution = Solution141()
 
     # [3,2,0,-4] pos=1
     head1 = ListNode(3)
@@ -32,17 +32,17 @@ if __name__ == "__main__":
     head1.next.next = ListNode(0)
     head1.next.next.next = ListNode(-4)
     head1.next.next.next.next = cycle_pos1
-    print(solution141.hasCycle(head1))
+    print(solution.hasCycle(head1))
 
     # [1,2] pos=0
     head2 = ListNode(1)
     head2.next = ListNode(2)
     head2.next.next = head2
-    print(solution141.hasCycle(head2))
+    print(solution.hasCycle(head2))
 
     # [1] pos=-1
     head3 = ListNode(1)
-    print(solution141.hasCycle(head3))
+    print(solution.hasCycle(head3))
 
 
 
