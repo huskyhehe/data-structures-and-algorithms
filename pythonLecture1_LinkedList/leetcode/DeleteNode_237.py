38ce22186b327616d3b0d255c87e3e9247a5da48
unknown <zhou.he1@northeastern.edu>
2022-09-16 11:57:19 -0700

    Add 237 solution

--- pythonLecture1_LinkedList/leetcode/DeleteNode_237.py
+++ pythonLecture1_LinkedList/leetcode/DeleteNode_237.py
@@ -1,7 +1,33 @@
-class Solution:
-    def deleteNode(self, node):
+from leetcode.ListNode import ListNode
+
+
+class Solution237:
+    def deleteNode(self, node: ListNode):
         succ = node.next
         node.val = succ.val
 
         node.next = succ.next
         succ.next = None
+
+
+if __name__ == "__main__":
+
+    solution237 = Solution237()
+
+    def printLinkedList(head: ListNode):
+        while head:
+            print(str(head.val) + " -> ", end="")
+            head = head.next
+        print("None")
+
+    # head = [4,5,1,9], node = 5
+    head1 = ListNode(4)
+    node_to_delete1 = ListNode(5)
+    head1.next = node_to_delete1
+    head1.next.next = ListNode(1)
+    head1.next.next.next = ListNode(9)
+    printLinkedList(head1)
+    solution237.deleteNode(node_to_delete1)
+    printLinkedList(head1)
+
+
