f6a39cfcf3c31407d63d5160263e75b9a65f0a56
unknown <zhou.he1@northeastern.edu>
2022-09-16 13:56:15 -0700

    Add 24 solution

--- pythonLecture1_LinkedList/leetcode/Swap_24.py
+++ pythonLecture1_LinkedList/leetcode/Swap_24.py
@@ -4,6 +4,7 @@ from leetcode.ListNode import ListNode
 
 
 class Solution24:
+    #
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
@@ -11,7 +12,36 @@ class Solution24:
         first_node = head
         second_node = head.next
 
-        first_node = self.swapPairs(second_node.next)
-        second_node = first_node.next
+        first_node.next = self.swapPairs(second_node.next)
+        second_node.next = first_node
 
         return second_node
+
+
+if __name__ == "__main__":
+
+    solution = Solution24()
+
+    def printLinkedList(head: ListNode):
+        while head:
+            print(str(head.val) + " -> ", end="")
+            head = head.next
+        print("None")
+
+    # [1,2,3,4]
+    head1 = ListNode(1)
+    head1.next = ListNode(2)
+    head1.next.next = ListNode(3)
+    head1.next.next.next = ListNode(4)
+    printLinkedList(head1)
+    printLinkedList(solution.swapPairs(head1))
+
+    # [] -- corner case
+    head2 = None
+    printLinkedList(head2)
+    printLinkedList(solution.swapPairs(head2))
+
+    # [1] -- corner case
+    head3 = ListNode(1)
+    printLinkedList(head3)
+    printLinkedList(solution.swapPairs(head3))
