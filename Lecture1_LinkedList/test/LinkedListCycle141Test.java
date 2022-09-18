a28512b898417202abdb27ddb045899c61f15540
unknown <zhou.he1@northeastern.edu>
2022-09-18 11:34:13 -0700

    Add test for 141

--- Lecture1_LinkedList/test/LinkedListCycle141Test.java
+++ Lecture1_LinkedList/test/LinkedListCycle141Test.java
@@ -7,30 +7,30 @@ public class LinkedListCycle141Test {
 
     @Test
     public void testHasCycle_pos1() {
-        LinkedListCycle141 list = new LinkedListCycle141();
+        LinkedListCycle141 solution = new LinkedListCycle141();
         ListNode head = new ListNode(3);
         ListNode node1 = new ListNode(2);
         head.next = node1;
         head.next.next = new ListNode(0);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = node1;
-        Assertions.assertEquals(true, list.hasCycle(head));
+        Assertions.assertEquals(true, solution.hasCycle(head));
     }
 
     @Test
     public void testHasCycle_pos0() {
-        LinkedListCycle141 list = new LinkedListCycle141();
+        LinkedListCycle141 solution = new LinkedListCycle141();
         ListNode head = new ListNode(1);
         ListNode node1 = new ListNode(2);
         head.next = node1;
         node1.next = head;
-        Assertions.assertEquals(true, list.hasCycle(head));
+        Assertions.assertEquals(true, solution.hasCycle(head));
     }
 
     @Test
     public void testHasCycle_no_cycle() {
-        LinkedListCycle141 list = new LinkedListCycle141();
+        LinkedListCycle141 solution = new LinkedListCycle141();
         ListNode head = new ListNode(1);
-        Assertions.assertEquals(false, list.hasCycle(head));
+        Assertions.assertEquals(false, solution.hasCycle(head));
     }
 }
