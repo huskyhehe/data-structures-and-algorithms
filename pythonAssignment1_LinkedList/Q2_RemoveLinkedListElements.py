a963418ca10a6f69f16071f19bd665c0d0195a56
unknown <zhou.he1@northeastern.edu>
2022-09-22 17:50:25 -0700

    Add Q2

---
+++ pythonAssignment1_LinkedList/Q2_RemoveLinkedListElements.py
@@ -0,0 +1,64 @@
+from typing import Optional
+
+from ListNode import ListNode
+
+
+class Solution2:
+    # https://leetcode.com/problems/remove-linked-list-elements
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+
+        if not head:
+            return None
+
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        pre = dummy
+        cur = head
+
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+            else:
+                pre = pre.next
+            cur = cur.next
+
+        return dummy.next
+
+    # dummy head technique & linked list traversal
+    # time: O(n)
+    # space: O(1)
+
+
+if __name__ == "__main__":
+    solution2 = Solution2()
+
+    def print_linked_list(head: ListNode):
+        while head:
+            print(str(head.val) + " -> ", end="")
+            head = head.next
+        print("None")
+
+    # Test Case 1
+    # 1 -> 2 -> 6 -> 3 -> 6 -> None, 6
+    # output: 1 -> 2 -> 3 -> None
+    head1 = ListNode(1)
+    head1.next = ListNode(2)
+    head1.next.next = ListNode(6)
+    head1.next.next.next = ListNode(3)
+    head1.next.next.next.next = ListNode(6)
+    print_linked_list(solution2.removeElements(head1, 6))
+
+    # Test Case 2
+    # input: 7 -> 7 -> 7 -> None, 7
+    # output: None
+    head2 = ListNode(7)
+    head2.next = ListNode(7)
+    head2.next.next = ListNode(7)
+    print_linked_list(solution2.removeElements(head2, 7))
+
+    # Test Case 3 (Edge Case)
+    # input: None, 1
+    # output: None
+    head3 = None
+    print_linked_list(solution2.removeElements(head2, 1))
