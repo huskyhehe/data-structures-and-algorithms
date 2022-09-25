c56a62e6599dc03cf3846980de485b997b25ea37
unknown <zhou.he1@northeastern.edu>
2022-09-25 14:48:46 -0700

    Add Q1

--- pythonAssignment1_LinkedList/Q1_RotateList.py
+++ pythonAssignment1_LinkedList/Q1_RotateList.py
@@ -42,24 +42,24 @@ if __name__ == "__main__":
 
     # Test Case 1
     # input: 1 -> 2 -> 3 -> 4 -> 5 -> None, 2
-    # output: 4 -> 5 -> 1 -> 2 -> 3 -> None
     head1 = ListNode(1)
     head1.next = ListNode(2)
     head1.next.next = ListNode(3)
     head1.next.next.next = ListNode(4)
     head1.next.next.next.next = ListNode(5)
+    # output: 4 -> 5 -> 1 -> 2 -> 3 -> None
     print_linked_list(solution1.rotateRight(head1, 2))
 
     # Test Case 2
     # input: 7 -> 8 -> 9 -> None, 5
-    # output: 8 -> 9 -> 7 -> None
     head2 = ListNode(7)
     head2.next = ListNode(8)
     head2.next.next = ListNode(9)
+    # output: 8 -> 9 -> 7 -> None
     print_linked_list(solution1.rotateRight(head2, 5))
 
     # Test Case 3 (Edge Case)
     # input: 3 -> None, 3
-    # output: 3 -> None
     head3 = ListNode(3)
+    # output: 3 -> None
     print_linked_list(solution1.rotateRight(head3, 3))
