387b525b01576eaf9c29109d8638a6a365749e8e
unknown <zhou.he1@northeastern.edu>
2022-10-10 16:31:26 -0700

    Add lecture3

--- pythonLecture3_SortSearch/covered_questions.py
+++ pythonLecture3_SortSearch/covered_questions.py
@@ -59,18 +59,46 @@ def majority_element(nums: List[int]) -> int:
     return candidate
 
 
+# two pointers + traverse from tail
+# time: O(m + n)
+# space: O(1)
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    p1 = m - 1
+    p2 = n - 1
+    for i in range(m + n - 1, -1, -1):
+        if p2 < 0:
+            break
+        if p1 >= 0 and nums1[p1] >= nums1[p2]:
+            nums1[i] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[i] = nums2[p2]
+            p2 -= 2
+
+
 if __name__ == "__main__":
     nums1 = [3, 2, 3, 1, 2, 4, 5, 5, 6]
     k1 = 4
     print("kth largest: ")
+    # expected: 4
     print(find_kth_largest(nums1, k1))
 
     nums2 = [1, 2, 3, 4, 5, 6, 7]
     k2 = 3
     rotate_reverse(nums2, 3)
     print("rotate:")
+    # expected: [5, 6, 7, 1, 2, 3, 4]
     print(nums2)
 
     nums3 = [2, 2, 1, 1, 1, 2, 2]
     print("majority element: ")
+    # expected: 2
     print(majority_element(nums3))
+
+    nums4 = [0]
+    m = 0
+    nums5 = [1]
+    n = 1
+    # expected: [1]
+    merge(nums4, 0, nums5, 1)
+    print(nums4)
