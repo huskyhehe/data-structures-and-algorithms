e9671a00bd9a677deac62c4bcdd07898a51641b8
unknown <zhou.he1@northeastern.edu>
2022-10-20 17:45:56 -0700

    Add Q6

--- pythonAssignment3_Backtracking/Q6_Subsets.py
+++ pythonAssignment3_Backtracking/Q6_Subsets.py
@@ -1,1 +1,26 @@
 # https://leetcode.com/problems/subsets/
+
+# time: O(2^n * n)
+# space: O(n)
+
+from typing import List
+
+
+class Solution6:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+
+        def backtracking(start: int, subset: List[int]):
+            if len(subset) <= n:
+                res.append(subset.copy())
+
+            for i in range(start, n):
+                subset.append(nums[i])
+                backtracking(i + 1, subset)
+                subset.pop()
+
+        backtracking(0, [])
+        return res
+
+
