739a55e4de691843b18d48888a5f1892ce838a21
unknown <zhou.he1@northeastern.edu>
2022-10-20 19:58:03 -0700

    Add Q5

--- pythonAssignment3_Backtracking/Q5_SameConsecutiveDifferences.py
+++ pythonAssignment3_Backtracking/Q5_SameConsecutiveDifferences.py
@@ -1,1 +1,34 @@
 # https://leetcode.com/problems/numbers-with-same-consecutive-differences/
+
+# using set() to avoid duplicates
+# time: O(2^n)
+# space: O(2^n)
+
+from typing import List
+
+
+class Solution5:
+    def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+
+        if N == 1:
+            return [i for i in range(10)]
+
+        res = []
+
+        def dfs(start, num):
+            if start == 0:
+                return res.append(num)
+
+            tail_digit = num % 10
+            # using set() to avoid duplicates when K == 0
+            next_digits = {tail_digit + K, tail_digit - K}
+
+            for digit in next_digits:
+                if 0 <= digit <= 9:
+                    new_num = num * 10 + digit
+                    dfs(start - 1, new_num)
+
+        for i in range(1, 10):
+            dfs(N - 1, i)
+
+        return res
