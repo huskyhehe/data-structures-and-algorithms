e852699ae922b286d95bf216ca61335077c7085b
unknown <zhou.he1@northeastern.edu>
2022-10-20 17:50:33 -0700

    Add Q7

--- pythonAssignment3_Backtracking/Q7_GenerateParentheses.py
+++ pythonAssignment3_Backtracking/Q7_GenerateParentheses.py
@@ -1,1 +1,30 @@
 # https://leetcode.com/problems/generate-parentheses/
+
+# time:
+# space:
+
+from typing import List
+
+
+class Solution7:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+
+        def backtracking(left: int, right: int, combo: List[str]):
+            if len(combo) == n * 2:
+                res.append("".join(combo))
+                return
+
+            if left < n:
+                combo.append("(")
+                backtracking(left + 1, right, combo)
+                combo.pop()
+
+            if left > right:
+                combo.append(")")
+                backtracking(left, right + 1, combo)
+                combo.pop()
+
+        backtracking(0, 0, [])
+        return res
