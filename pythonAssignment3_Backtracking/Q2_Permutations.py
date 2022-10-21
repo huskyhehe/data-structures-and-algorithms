9435db889a974b5c41682a64d4b0db1a5b9480e8
unknown <zhou.he1@northeastern.edu>
2022-10-20 17:37:29 -0700

    Add Q2

--- pythonAssignment3_Backtracking/Q2_Permutations.py
+++ pythonAssignment3_Backtracking/Q2_Permutations.py
@@ -1,1 +1,23 @@
 # https://leetcode.com/problems/permutations/
+
+# time: O(n×n!)
+# space: O(n)
+
+from typing import List
+
+
+class Solution2:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+
+        def backtracking(start: int):
+            if start == n:
+                res.append(nums.copy())
+            for i in range(start, n):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtracking(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        backtracking(0)
+        return res
