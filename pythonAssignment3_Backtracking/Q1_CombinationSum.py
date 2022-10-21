48761083369f3d47d8895ca8389e2228923f600e
unknown <zhou.he1@northeastern.edu>
2022-10-20 18:45:01 -0700

    Add Q1

--- pythonAssignment3_Backtracking/Q1_CombinationSum.py
+++ pythonAssignment3_Backtracking/Q1_CombinationSum.py
@@ -1,1 +1,28 @@
 # https://leetcode.com/problems/combination-sum/
+
+# time: O( n ^ (t/m + 1))       t = target, n = len(candidates)
+# space: O(t/m)
+
+from typing import List
+
+
+class Solution1:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtracking(start: int, remain: int, comb: List[int]) -> None:
+            if remain == 0:
+                res.append(comb.copy())
+                return
+
+            if remain <= 0:
+                return
+
+            for i in range(start, len(candidates)):
+                comb.append(candidates[i])
+                backtracking(i, remain - candidates[i], comb)
+                comb.pop()
+
+        backtracking(0, target, [])
+        return res
+
