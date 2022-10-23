06594eba02811b8df39c7992b6c275e75910d44e
unknown <zhou.he1@northeastern.edu>
2022-10-23 13:15:03 -0700

    Add lecture6

---
+++ pythonLecture6_BinarySearchTree/BinarySearchTree.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from TreeNode import TreeNode
+
+
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
+
+    # validate
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+    # basic operation:
+    # search
+    # insert
+    # delete
+
+    # search
+    def searchInBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+    # insert
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+    # delete
+    def deleteNodeInBST(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+
+
+
+
